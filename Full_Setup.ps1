b4ed5e720a1a7682d3f307f94b968ce353068b57
David Hasenhüttl <hasi1999@gmx.at>
2024-04-14 02:42:36 +0200

    Fixes for executionPolicy

--- Full_Setup.ps1
+++ Full_Setup.ps1
@@ -2,15 +2,17 @@
 
 # Parameter to prevent infinite recursion loop
 param(
-    [switch]$elevated = $false,
-    [string]$executionPolicy = $false
+    [switch]$elevated = $false
 )
 
+. $PSScriptRoot\Install\PowerShell\Functions.ps1
+
+$executionPolicy = Get-ExecutionPolicy -Scope CurrentUser
 # Ensure ExecutionPolicy Policy is set to RemoteSigned
-if ($executionPolicy -eq $false) {
-    $executionPolicy = Get-ExecutionPolicy -Scope CurrentUser
+if ($executionPolicy -eq "Restricted") {
+    Wait-For-Keypress -message "`nEnsure ExecutionPolicy Policy is set to RemoteSigned! Command:`n
+    Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force`n`nPress any key to close..." -color "Red"
 }
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
 
 # Ensure this script is run as admin
 function Test-Admin {
@@ -22,32 +24,24 @@ if ((Test-Admin) -eq $false)  {
     if ($elevated) {
         # Tried to elevate, did not work, aborting to prevent recursion loop
     } else {
-        Start-Process powershell.exe -Verb RunAs -ArgumentList "-noprofile", "-file", $myinvocation.MyCommand.Definition, "-elevated", "-executionPolicy", $executionPolicy
+        Start-Process powershell.exe -Verb RunAs -ArgumentList "-noprofile", "-file", $myinvocation.MyCommand.Definition, "-elevated"
     }
     exit
 }
 
 # Run installation functions, print error on fail
 try {
-    . $PSScriptRoot\Install\PowerShell\Functions.ps1
-    # . $PSScriptRoot\Install\PowerShell\Install_WSL.ps1
+    . $PSScriptRoot\Install\PowerShell\Install_WSL.ps1
 
 } catch {
     $ErrorMessage = $_.Exception.Message
-
-    # Reset ExecutionPolicy Policy
-    Set-ExecutionPolicy -ExecutionPolicy $executionPolicy -Scope CurrentUser -Force
 
     Wait-For-Keypress -message "`n$ErrorMessage`n`nPress any key to close..." -color "Red"
     exit
 }
 
-# Reset ExecutionPolicy Policy
-Set-ExecutionPolicy -ExecutionPolicy $executionPolicy -Scope CurrentUser -Force
-
 Wait-For-Keypress -message "`nInstallation successful!`n`nPress any key to close..." -color "Green"
 exit
-
 
 # Known Issues:
 # export TERM=xterm in case of problems

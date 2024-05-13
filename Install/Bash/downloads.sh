94c1061d9df275d529e148879e236f72bc590367
hasenhuettl <56801070+hasenhuettl@users.noreply.github.com>
2024-05-13 17:03:56 +0200

    Proper file name matching

--- Install/Bash/downloads.sh
+++ Install/Bash/downloads.sh
@@ -4,9 +4,9 @@ set -e
 
 sudo apt update
 sudo apt install tmux zsh sshpass kinit krb5-user tldr
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="daveverwer"/' ~/.zshrc
-mv ~/custom.zsh ~/.oh-my-zsh/custom/custom.zsh
+mv ~/.custom.zsh ~/.oh-my-zsh/custom/custom.zsh
 source .zshrc
 sudo ln -s ~/custom/sshurl.pl /usr/bin/sshurl
 

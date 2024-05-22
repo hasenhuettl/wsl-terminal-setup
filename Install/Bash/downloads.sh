1519022210c31d0d6f3910e6be01dfeb6e576d00
David Hasenhüttl <hasi1999@gmx.at>
2024-05-22 06:51:51 +0200

    apt install skip prompt

--- Install/Bash/downloads.sh
+++ Install/Bash/downloads.sh
@@ -3,7 +3,7 @@
 set -e
 
 sudo apt update
-sudo apt install tmux zsh sshpass tldr
+sudo apt install tmux zsh sshpass tldr -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="daveverwer"/' ~/.zshrc
 mv ~/.custom.zsh ~/.oh-my-zsh/custom/custom.zsh

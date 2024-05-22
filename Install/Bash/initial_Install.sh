06863d2f9c7982833a5c74883552a66e3358d55f
hasenhuettl <56801070+hasenhuettl@users.noreply.github.com>
2024-05-22 07:14:43 +0200

    always setup dns

--- Install/Bash/initial_Install.sh
+++ Install/Bash/initial_Install.sh
@@ -11,9 +11,9 @@ newhostname="$(whoami)-wsl"
 tar xf /mnt/c/…/tmux.tar.bz2
 echo -e "[network]\nhostname=${newhostname}\ngenerateHosts=false" | sudo tee /etc/wsl.conf
 
-#Optional - set DNS server ips: 
-#echo -e "nameserver 1.1.1.1\nnameserver 4.4.4.4\n" | sudo tee /etc/resolv.conf
-#echo -e "\ngenerateResolvConf=false" | sudo tee -a /etc/wsl.conf
+# Set DNS server ips: 
+echo -e "nameserver 1.1.1.1\nnameserver 4.4.4.4\n" | sudo tee /etc/resolv.conf
+echo -e "\ngenerateResolvConf=false" | sudo tee -a /etc/wsl.conf
 
 sudo sed -i "s/$oldhostname/$newhostname/g" /etc/hosts
 sed -i "s/replace_with_your_hostname/$newhostname/g" /etc/hosts

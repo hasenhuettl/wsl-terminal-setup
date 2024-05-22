b6d837a781358958d0403256fac71d05fdbf853a
David Hasenhüttl <hasi1999@gmx.at>
2024-05-22 07:14:43 +0200

    always setup dns

--- Install/Bash/downloads.sh
+++ Install/Bash/downloads.sh
@@ -6,8 +6,8 @@ sudo apt update
 sudo apt install tmux zsh sshpass tldr -y
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="daveverwer"/' ~/.zshrc
-mv ~/.custom.zsh ~/.oh-my-zsh/custom/custom.zsh
-source .zshrc
+sudo ln -s ~/custom/ssh/.custom.zsh ~/.oh-my-zsh/custom/custom.zsh
 sudo ln -s ~/custom/sshurl.pl /usr/bin/sshurl
 
+# reboot (wsl --shutdown)
 # exit

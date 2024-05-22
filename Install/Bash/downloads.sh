78f9c8640abc6083d4af7c2a8adbb8b9fea3bebb
hasenhuettl <56801070+hasenhuettl@users.noreply.github.com>
2024-05-22 06:30:49 +0200

    Removed Kerberos Auth packages

--- Install/Bash/downloads.sh
+++ Install/Bash/downloads.sh
@@ -3,7 +3,7 @@
 set -e
 
 sudo apt update
-sudo apt install tmux zsh sshpass kinit krb5-user tldr
+sudo apt install tmux zsh sshpass tldr
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="daveverwer"/' ~/.zshrc
 mv ~/.custom.zsh ~/.oh-my-zsh/custom/custom.zsh

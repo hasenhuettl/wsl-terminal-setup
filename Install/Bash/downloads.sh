4235c2901a500b15b12357bceb093b3c9532bab6
hasenhuettl <56801070+hasenhuettl@users.noreply.github.com>
2025-06-25 23:14:01 +0200

    replaced snap with flatpak, first steps to also support linux systems

--- Install/Bash/downloads.sh
+++ Install/Bash/downloads.sh
@@ -5,26 +5,38 @@ set -e
 sudo apt update
 sudo apt install tmux bash zsh sshpass -y # Terminal handling
 sudo apt install tldr wget git gh -y # Useful tools
-sudo apt install snapd fzf fd-find ripgrep luarocks -y # Neovim setup
-sudo systemctl enable --now snapd.socket
+sudo apt install fzf fd-find ripgrep luarocks -y # Neovim dependencies
 
-# Wait for snapd to be fully initialized
-echo "Waiting for snapd to be ready..."
-until snap version &>/dev/null; do
-    echo "snapd not ready, waiting 5 seconds..."
-    sleep 5
-done
+# # Install nvim using snap
+# sudo apt install snapd
+# sudo systemctl enable --now snapd.socket
+#
+# # Wait for snapd to be fully initialized
+# echo "Waiting for snapd to be ready..."
+# until snap version &>/dev/null; do
+#     echo "snapd not ready, waiting 5 seconds..."
+#     sleep 5
+# done
+#
+# echo "snapd is ready, installing nvim..."
+# sudo snap install nvim --classic
 
-# Step 5: Install nvim using snap
-echo "snapd is ready, installing nvim..."
-sudo snap install nvim --classic
+# Install nvim using flatpak
+sudo apt install flatpak -y
+flatpak install flathub io.neovim.nvim -y
 
-mkdir -p ~/.config
-sudo mkdir -p /opt/git
-sudo chown $USER:$USER /opt/git
-cd /opt/git
-git clone https://github.com/hasenhuettl/wsl-terminal-setup.git
-ln -s /opt/git/wsl-terminal-setup/Files/custom ~/custom
+cd ~
+mkdir -p .config
+sudo mkdir -p git
+cd git
+
+FOLDER=~/git/wsl-terminal-setup
+
+if [ ! -d "$FOLDER" ] ; then
+  git clone https://github.com/hasenhuettl/wsl-terminal-setup.git
+fi
+
+ln -s $FOLDER/Files/custom ~/custom
 
 cd
 
@@ -35,13 +47,14 @@ sed -i '/export ZSH="\$HOME\/.oh-my-zsh"/i export ZSH_COMPDUMP=$ZSH/cache/.zcomp
 sed -i 's/ZSH_THEME="[^"]*"/ZSH_THEME="daveverwer"/' ~/.zshrc
 
 # symlink all !FILES AND SYMLINKS! that start with . in given path to ~/
-find /opt/git/wsl-terminal-setup/Files/ -maxdepth 1 -type f,l -name ".*" -exec ln -s {} ~/ \;
+find $FOLDER/Files/ -maxdepth 1 -type f,l -name ".*" -exec ln -s {} ~/ \;
 
 rm -rf ~/.oh-my-zsh/custom
 # copy all !DIRECTORIES! that start with . in given path to ~/
-find /opt/git/wsl-terminal-setup/Files/ -maxdepth 1 -type d -name ".*" -exec cp -a {} ~/ \;
+find $FOLDER/Files/ -maxdepth 1 -type d -name ".*" -exec cp -a {} ~/ \;
 
-ln -s ~/custom/sshurl.pl /usr/bin/sshurl
+sudo ln -s ~/custom/sshurl.pl /usr/bin/sshurl
 
-# reboot (wsl --shutdown)
+# reboot, or on WSL open cmd and execute: wsl --shutdown
 # exit
+

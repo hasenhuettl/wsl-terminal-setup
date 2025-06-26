c15e636b05d4776780b7a3f955f50afecb6515cd
hasenhuettl <56801070+hasenhuettl@users.noreply.github.com>
2025-06-26 17:46:18 +0200

    Now opening tmux within bash as login shell

--- Files/custom/ssh/.config/nvim/lua/plugins/oil.lua
+++ Files/custom/ssh/.config/nvim/lua/plugins/oil.lua
@@ -10,7 +10,20 @@ return {
 	lazy = false,
 
 	config = function()
-		require("oil").setup({})
+		require("oil").setup({
+			view_options = {
+				show_hidden = true,
+				-- This function defines what is considered a "hidden" file
+				is_hidden_file = function(name, bufnr)
+					local m = name:match("^%.")
+					return m ~= nil
+				end,
+				-- This function defines what will never be shown, even when `show_hidden` is set
+				is_always_hidden = function(name, bufnr)
+					return false
+				end
+			}
+		})
 		vim.keymap.set("n", "-", "<CMD>Oil --float<CR>", { desc = "Open parent directory" })
 	end,
 }
--- Files/custom/ssh/.oh-my-zsh/custom/00_custom.zsh
+++ Files/custom/ssh/.oh-my-zsh/custom/00_custom.zsh
@@ -1,16 +1,14 @@
 umask 0002
 
 alias ls='ls -hF --color=tty'
-alias vi='nvim'
-alias vim="vim -u ~/.myvimrc"
+alias vi="vim -u ~/.myvimrc"
+alias vim='nvim'
 
 alias repo="cd /var/www/html/git/dev/prototypes/"
 alias gits="git status"
 alias gitk="git log --graph --pretty=format:'%C(yellow)%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 alias gitkk="git log --all --decorate --oneline --graph"
 alias gitstats="git shortlog -sne --no-merges"
-alias ssh=~/custom/myssh.sh
-alias ussh=/usr/bin/ssh
 
 bindkey "^[[1;5C" forward-word
 bindkey "^[[1;5D" backward-word
@@ -21,8 +19,9 @@ bindkey -s '^[[1;2C' ''
 bindkey -s '^[[1;2D' ''
 
 function parse_git_branch() {
-    git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/[\1] /p'
+  git branch 2> /dev/null | sed -n -e 's/^\* \(.*\)/[\1] /p'
 }
+
 
 # Refer to: https://www.ditig.com/publications/256-colors-cheat-sheet
 COLOR_DEF=$'%f'
@@ -35,6 +34,13 @@ setopt PROMPT_SUBST
 
 export PROMPT='${COLOR_USR}%n${COLOR_ATS}@${COLOR_SRV}%m ${COLOR_DIR}%3~ ${COLOR_GIT}$(parse_git_branch)${COLOR_DEF}$ '
 
+
 export HISTIGNORE=' *'
 
 export GOPATH=$HOME/go
+
+if [[ $(hostname) == *-wsl ]]; then
+  alias ssh=~/custom/myssh.sh
+  alias ussh=/usr/bin/ssh
+fi
+
